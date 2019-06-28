32935bf78c0872758cf07268646e96a621dfe365
Калентьев Константин Анатольевич <kalentyev_ka@tomsk.lemz.ru>
2019-06-28 11:24:37 +0700

    Добавил проект c#

---
+++ CSharp/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+	public class Circle : IFigure
+	{
+		private double _radius;
+
+		public double Angles { get; }
+		public double Radius {
+			get
+			{
+				return _radius;
+			}
+			set
+			{
+				if(value <= 0)
+				{
+					throw new ArgumentException("Введенное значение меньше или равно 0");
+				}
+				else
+				{
+					_radius = value;
+				}
+			}
+		}
+
+		public Circle(double radius)
+		{
+			_radius = radius;
+		}
+
+		public double CalculatePerimeter()
+		{
+			return 2 * Math.PI * _radius;
+		}
+
+		public double CalculateSurface()
+		{
+			return Math.PI * _radius * _radius;
+		}
+	}
+}
---
+++ ConsoleLoader/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace ConsoleLoader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle first = new Circle(15);
+
+			Console.WriteLine("Площадь круга: {0}", first.CalculateSurface());
+			Console.WriteLine("Периметр круга: {0}", first.CalculatePerimeter());
+
+			Console.ReadKey();
+		}
+	}
+}
