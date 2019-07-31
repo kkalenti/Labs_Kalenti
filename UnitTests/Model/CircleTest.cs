1a5cb15271301c03dd5d0f84bd20bd59407c8bdb
Калентьев Константин Анатольевич <kalentyev_ka@tomsk.lemz.ru>
2019-07-31 11:38:19 +0700

    Добавил часть с тестами

---
+++ UnitTests/Model/CircleTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using NUnit.Framework;
+
+namespace UnitTests.Model
+{
+	/// <summary>
+	/// Набор тестов класса Circle
+	/// </summary>
+	[TestFixture]
+	class CircleTest
+	{
+		public void SurfaceTest(double surface)
+		{
+			var circle = new Circle();
+		}
+	}
+}
